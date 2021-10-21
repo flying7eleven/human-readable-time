13ce2ad21350bc8877f6df7644d400fadbf967d0
Tim Janke <tim+github@janke.biz>
2021-10-21 13:45:24 +0200

    :sparkles: Implement FromStr for the data structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-use std::convert::TryFrom;
+use std::fmt::{Debug, Display, Formatter};
+use std::str::FromStr;
 
 /// A data structure for parsing and managing a human readable duration representation
 pub struct HumanReadableDuration {
@@ -10,10 +11,10 @@ impl HumanReadableDuration {
     ///
     /// # Example
     /// ```
-    /// use std::convert::TryFrom;
+    /// use std::str::FromStr;
     /// use human_readable_time::HumanReadableDuration;
     ///
-    /// let duration = HumanReadableDuration::try_from("10s");
+    /// let duration = HumanReadableDuration::from_str("10s");
     ///
     /// assert_eq!(10, duration.unwrap().seconds());
     /// ```
@@ -22,10 +23,27 @@ impl HumanReadableDuration {
     }
 }
 
-impl TryFrom<&str> for HumanReadableDuration {
-    type Error = ();
+/// The error which will be returned, if a value could not be parsed into an `HumanReadableDuration`
+pub struct ParseHumanReadableDurationError;
 
-    fn try_from(value: &str) -> Result<Self, Self::Error> {
+impl Debug for ParseHumanReadableDurationError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "ParseHumanReadableDurationError")
+    }
+}
+
+impl Display for ParseHumanReadableDurationError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "ParseHumanReadableDurationError")
+    }
+}
+
+impl std::error::Error for ParseHumanReadableDurationError {}
+
+impl FromStr for HumanReadableDuration {
+    type Err = ParseHumanReadableDurationError;
+
+    fn from_str(value: &str) -> Result<Self, Self::Err> {
         Ok(HumanReadableDuration {
             time_in_seconds: value.split("s").next().unwrap().parse::<u64>().unwrap(),
         })
@@ -35,25 +53,25 @@ impl TryFrom<&str> for HumanReadableDuration {
 #[cfg(test)]
 mod tests {
     use crate::HumanReadableDuration;
-    use std::convert::TryFrom;
+    use std::str::FromStr;
 
     #[test]
-    fn from_10s_works() {
-        let representation = HumanReadableDuration::try_from("10s");
+    fn from_str_10s_works() {
+        let representation = HumanReadableDuration::from_str("10s");
         assert_eq!(true, representation.is_ok());
         assert_eq!(10, representation.unwrap().seconds());
     }
 
     #[test]
-    fn from_60s_works() {
-        let representation = HumanReadableDuration::try_from("60");
+    fn from_str_60s_works() {
+        let representation = HumanReadableDuration::from_str("60");
         assert_eq!(true, representation.is_ok());
         assert_eq!(60, representation.unwrap().seconds());
     }
 
     #[test]
-    fn from_61s_works() {
-        let representation = HumanReadableDuration::try_from("61s");
+    fn from_str_61s_works() {
+        let representation = HumanReadableDuration::from_str("61s");
         assert_eq!(true, representation.is_ok());
         assert_eq!(61, representation.unwrap().seconds());
     }
