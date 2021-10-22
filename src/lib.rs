1e082c7380dca19d1ce26b5534fd96d284a178c0
Tim Janke <tim+github@janke.biz>
2021-10-22 07:22:23 +0200

    :memo: Add a lot more documentation

--- src/lib.rs
+++ src/lib.rs
@@ -43,13 +43,22 @@ impl HumanReadableDuration {
 /// The error which will be returned, if a value could not be parsed into an `HumanReadableDuration`
 pub struct ParseHumanReadableDurationError;
 
+/// `?` formatting.
+///
+/// `Debug` should format the output in a programmer-facing, debugging context.
 impl Debug for ParseHumanReadableDurationError {
+    /// Formats the value using the given formatter.
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         write!(f, "ParseHumanReadableDurationError")
     }
 }
 
+/// Format trait for an empty format, `{}`.
+///
+/// `Display` is similar to [`Debug`], but `Display` is for user-facing
+/// output.
 impl Display for ParseHumanReadableDurationError {
+    /// Formats the value using the given formatter.
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         write!(f, "ParseHumanReadableDurationError")
     }
@@ -57,9 +66,29 @@ impl Display for ParseHumanReadableDurationError {
 
 impl std::error::Error for ParseHumanReadableDurationError {}
 
+/// Parse a value from a string
+///
+/// `FromStr`'s `from_str` method is often used implicitly, through
+/// `str`'s `parse` method. See `parse`'s documentation for examples.
 impl FromStr for HumanReadableDuration {
     type Err = ParseHumanReadableDurationError;
 
+    /// Parses a string `s` to return a value of the [`HumanReadableDuration`] type.
+    ///
+    /// If parsing succeeds, return the value inside [`Ok`], otherwise
+    /// when the string is ill-formatted return an error specific to the
+    /// inside [`Err`].
+    ///
+    /// # Examples
+    /// ```
+    /// use std::str::FromStr;
+    /// use human_readable_time::HumanReadableDuration;
+    ///
+    /// let s = "50s";
+    /// let x = HumanReadableDuration::from_str(s).unwrap();
+    ///
+    /// assert_eq!(50, x.seconds());
+    /// ```
     fn from_str(value: &str) -> Result<Self, Self::Err> {
         Ok(HumanReadableDuration {
             time_in_seconds: value.split("s").next().unwrap().parse::<u64>().unwrap(),
@@ -67,10 +96,68 @@ impl FromStr for HumanReadableDuration {
     }
 }
 
+/// Used to do value-to-value conversions while consuming the input value. It is the reciprocal of
+/// [`Into`].
+impl From<u64> for HumanReadableDuration {
+    /// Create an instance for [`HumanReadableDuration`] from a `u64` field representing the seconds
+    ///
+    /// # Example
+    /// ```
+    /// use human_readable_time::HumanReadableDuration;
+    ///
+    /// let seconds: u64 = 300;
+    /// let representation = HumanReadableDuration::from(seconds);
+    ///
+    /// assert_eq!(300, representation.seconds());
+    /// assert_eq!(5, representation.minutes());
+    /// ```
+    fn from(value: u64) -> Self {
+        HumanReadableDuration {
+            time_in_seconds: value,
+        }
+    }
+}
+
+/// Used to do value-to-value conversions while consuming the input value. It is the reciprocal of
+/// [`Into`].
+impl From<u32> for HumanReadableDuration {
+    /// Create an instance for [`HumanReadableDuration`] from a `u32` field representing the seconds
+    ///
+    /// # Example
+    /// ```
+    /// use human_readable_time::HumanReadableDuration;
+    ///
+    /// let seconds: u32 = 300;
+    /// let representation = HumanReadableDuration::from(seconds);
+    ///
+    /// assert_eq!(300, representation.seconds());
+    /// assert_eq!(5, representation.minutes());
+    /// ```
+    fn from(value: u32) -> Self {
+        HumanReadableDuration {
+            time_in_seconds: value as u64,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use crate::HumanReadableDuration;
     use std::str::FromStr;
+
+    #[test]
+    fn from_u32_works() {
+        let representation = HumanReadableDuration::from(300 as u32);
+        assert_eq!(300, representation.seconds());
+        assert_eq!(5, representation.minutes());
+    }
+
+    #[test]
+    fn from_u64_works() {
+        let representation = HumanReadableDuration::from(300 as u64);
+        assert_eq!(300, representation.seconds());
+        assert_eq!(5, representation.minutes());
+    }
 
     #[test]
     fn from_str_10s_works() {
