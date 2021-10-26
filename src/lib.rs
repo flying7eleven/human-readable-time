19a3465b27c2b5671e70eab86a709ecb4304b90a
Tim Janke <tim+github@janke.biz>
2021-10-26 11:43:55 +0200

    :recycle: Implement and use the AsSeconds and AsMinutes traits

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,47 @@
+use crate::traits::{AsMinutes, AsSeconds};
 use std::fmt::{Debug, Display, Formatter};
 use std::str::FromStr;
+
+// the modules we have in this crate
+pub mod traits;
 
 /// A data structure for parsing and managing a human readable duration representation
 pub struct HumanReadableDuration {
     time_in_seconds: u64,
 }
 
-impl HumanReadableDuration {
+impl AsSeconds for HumanReadableDuration {
     /// Get the duration time in seconds
     ///
     /// # Example
     /// ```
     /// use std::str::FromStr;
     /// use human_readable_time::HumanReadableDuration;
+    /// use human_readable_time::traits::AsSeconds;
     ///
     /// let duration = HumanReadableDuration::from_str("10s");
     ///
-    /// assert_eq!(10, duration.unwrap().seconds());
+    /// assert_eq!(10, duration.unwrap().as_seconds());
     /// ```
-    pub fn seconds(&self) -> u64 {
+    fn as_seconds(&self) -> u64 {
         self.time_in_seconds
     }
+}
 
+impl AsMinutes for HumanReadableDuration {
     /// Get the duration time in full minutes
     ///
     /// # Example
     /// ```
     /// use std::str::FromStr;
     /// use human_readable_time::HumanReadableDuration;
+    /// use human_readable_time::traits::AsMinutes;
     ///
     /// let duration = HumanReadableDuration::from_str("65s");
     ///
-    /// assert_eq!(1, duration.unwrap().minutes());
+    /// assert_eq!(1, duration.unwrap().as_minutes());
     /// ```
-    pub fn minutes(&self) -> u64 {
+    fn as_minutes(&self) -> u64 {
         let divisor = self.time_in_seconds as f32;
         let result = divisor / 60.0f32;
         return result as u64;
@@ -135,11 +143,12 @@ impl FromStr for HumanReadableDuration {
     /// ```
     /// use std::str::FromStr;
     /// use human_readable_time::HumanReadableDuration;
+    /// use human_readable_time::traits::AsSeconds;
     ///
     /// let s = "50s";
     /// let x = HumanReadableDuration::from_str(s).unwrap();
     ///
-    /// assert_eq!(50, x.seconds());
+    /// assert_eq!(50, x.as_seconds());
     /// ```
     fn from_str(value: &str) -> Result<Self, Self::Err> {
         // try to get the time information from the passed string
@@ -172,12 +181,13 @@ impl From<u64> for HumanReadableDuration {
     /// # Example
     /// ```
     /// use human_readable_time::HumanReadableDuration;
+    /// use human_readable_time::traits::{AsMinutes, AsSeconds};
     ///
     /// let seconds: u64 = 300;
     /// let representation = HumanReadableDuration::from(seconds);
     ///
-    /// assert_eq!(300, representation.seconds());
-    /// assert_eq!(5, representation.minutes());
+    /// assert_eq!(300, representation.as_seconds());
+    /// assert_eq!(5, representation.as_minutes());
     /// ```
     fn from(value: u64) -> Self {
         HumanReadableDuration {
@@ -194,12 +204,13 @@ impl From<u32> for HumanReadableDuration {
     /// # Example
     /// ```
     /// use human_readable_time::HumanReadableDuration;
+    /// use human_readable_time::traits::{AsMinutes, AsSeconds};
     ///
     /// let seconds: u32 = 300;
     /// let representation = HumanReadableDuration::from(seconds);
     ///
-    /// assert_eq!(300, representation.seconds());
-    /// assert_eq!(5, representation.minutes());
+    /// assert_eq!(300, representation.as_seconds());
+    /// assert_eq!(5, representation.as_minutes());
     /// ```
     fn from(value: u32) -> Self {
         HumanReadableDuration {
@@ -216,12 +227,13 @@ impl From<u16> for HumanReadableDuration {
     /// # Example
     /// ```
     /// use human_readable_time::HumanReadableDuration;
+    /// use human_readable_time::traits::{AsMinutes, AsSeconds};
     ///
     /// let seconds: u16 = 300;
     /// let representation = HumanReadableDuration::from(seconds);
     ///
-    /// assert_eq!(300, representation.seconds());
-    /// assert_eq!(5, representation.minutes());
+    /// assert_eq!(300, representation.as_seconds());
+    /// assert_eq!(5, representation.as_minutes());
     /// ```
     fn from(value: u16) -> Self {
         HumanReadableDuration {
@@ -238,12 +250,13 @@ impl From<u8> for HumanReadableDuration {
     /// # Example
     /// ```
     /// use human_readable_time::HumanReadableDuration;
+    /// use human_readable_time::traits::{AsMinutes, AsSeconds};
     ///
     /// let seconds: u8 = 120;
     /// let representation = HumanReadableDuration::from(seconds);
     ///
-    /// assert_eq!(120, representation.seconds());
-    /// assert_eq!(2, representation.minutes());
+    /// assert_eq!(120, representation.as_seconds());
+    /// assert_eq!(2, representation.as_minutes());
     /// ```
     fn from(value: u8) -> Self {
         HumanReadableDuration {
@@ -254,21 +267,22 @@ impl From<u8> for HumanReadableDuration {
 
 #[cfg(test)]
 mod tests {
+    use crate::traits::{AsMinutes, AsSeconds};
     use crate::HumanReadableDuration;
     use std::str::FromStr;
 
     #[test]
     fn from_u32_works() {
         let representation = HumanReadableDuration::from(300 as u32);
-        assert_eq!(300, representation.seconds());
-        assert_eq!(5, representation.minutes());
+        assert_eq!(300, representation.as_seconds());
+        assert_eq!(5, representation.as_minutes());
     }
 
     #[test]
     fn from_u64_works() {
         let representation = HumanReadableDuration::from(300 as u64);
-        assert_eq!(300, representation.seconds());
-        assert_eq!(5, representation.minutes());
+        assert_eq!(300, representation.as_seconds());
+        assert_eq!(5, representation.as_minutes());
     }
 
     #[test]
@@ -281,24 +295,24 @@ mod tests {
     fn from_str_10s_works() {
         let representation = HumanReadableDuration::from_str("10s");
         assert_eq!(true, representation.is_ok());
-        assert_eq!(10, representation.as_ref().unwrap().seconds());
-        assert_eq!(0, representation.as_ref().unwrap().minutes());
+        assert_eq!(10, representation.as_ref().unwrap().as_seconds());
+        assert_eq!(0, representation.as_ref().unwrap().as_minutes());
     }
 
     #[test]
     fn from_str_60s_works() {
         let representation = HumanReadableDuration::from_str("60s");
         assert_eq!(true, representation.is_ok());
-        assert_eq!(60, representation.as_ref().unwrap().seconds());
-        assert_eq!(1, representation.as_ref().unwrap().minutes());
+        assert_eq!(60, representation.as_ref().unwrap().as_seconds());
+        assert_eq!(1, representation.as_ref().unwrap().as_minutes());
     }
 
     #[test]
     fn from_str_61s_works() {
         let representation = HumanReadableDuration::from_str("61s");
         assert_eq!(true, representation.is_ok());
-        assert_eq!(61, representation.as_ref().unwrap().seconds());
-        assert_eq!(1, representation.as_ref().unwrap().minutes());
+        assert_eq!(61, representation.as_ref().unwrap().as_seconds());
+        assert_eq!(1, representation.as_ref().unwrap().as_minutes());
     }
 
     #[test]
@@ -311,24 +325,24 @@ mod tests {
     fn from_str_5m_works() {
         let representation = HumanReadableDuration::from_str("5m");
         assert_eq!(true, representation.is_ok());
-        assert_eq!(300, representation.as_ref().unwrap().seconds());
-        assert_eq!(5, representation.as_ref().unwrap().minutes());
+        assert_eq!(300, representation.as_ref().unwrap().as_seconds());
+        assert_eq!(5, representation.as_ref().unwrap().as_minutes());
     }
 
     #[test]
     fn from_str_60m_works() {
         let representation = HumanReadableDuration::from_str("60m");
         assert_eq!(true, representation.is_ok());
-        assert_eq!(3600, representation.as_ref().unwrap().seconds());
-        assert_eq!(60, representation.as_ref().unwrap().minutes());
+        assert_eq!(3600, representation.as_ref().unwrap().as_seconds());
+        assert_eq!(60, representation.as_ref().unwrap().as_minutes());
     }
 
     #[test]
     fn from_str_61m_works() {
         let representation = HumanReadableDuration::from_str("61m");
         assert_eq!(true, representation.is_ok());
-        assert_eq!(3660, representation.as_ref().unwrap().seconds());
-        assert_eq!(61, representation.as_ref().unwrap().minutes());
+        assert_eq!(3660, representation.as_ref().unwrap().as_seconds());
+        assert_eq!(61, representation.as_ref().unwrap().as_minutes());
     }
 
     #[test]
@@ -341,31 +355,31 @@ mod tests {
     fn from_str_4m_10s_works() {
         let representation = HumanReadableDuration::from_str("4m 10s");
         assert_eq!(true, representation.is_ok());
-        assert_eq!(250, representation.as_ref().unwrap().seconds());
-        assert_eq!(4, representation.as_ref().unwrap().minutes());
+        assert_eq!(250, representation.as_ref().unwrap().as_seconds());
+        assert_eq!(4, representation.as_ref().unwrap().as_minutes());
     }
 
     #[test]
     fn from_str_4m10s_works() {
         let representation = HumanReadableDuration::from_str("4m10s");
         assert_eq!(true, representation.is_ok());
-        assert_eq!(250, representation.as_ref().unwrap().seconds());
-        assert_eq!(4, representation.as_ref().unwrap().minutes());
+        assert_eq!(250, representation.as_ref().unwrap().as_seconds());
+        assert_eq!(4, representation.as_ref().unwrap().as_minutes());
     }
 
     #[test]
     fn from_str_3m60s_works() {
         let representation = HumanReadableDuration::from_str("3m60s");
         assert_eq!(true, representation.is_ok());
-        assert_eq!(240, representation.as_ref().unwrap().seconds());
-        assert_eq!(4, representation.as_ref().unwrap().minutes());
+        assert_eq!(240, representation.as_ref().unwrap().as_seconds());
+        assert_eq!(4, representation.as_ref().unwrap().as_minutes());
     }
 
     #[test]
     fn from_str_3m61s_works() {
         let representation = HumanReadableDuration::from_str("3m61s");
         assert_eq!(true, representation.is_ok());
-        assert_eq!(241, representation.as_ref().unwrap().seconds());
-        assert_eq!(4, representation.as_ref().unwrap().minutes());
+        assert_eq!(241, representation.as_ref().unwrap().as_seconds());
+        assert_eq!(4, representation.as_ref().unwrap().as_minutes());
     }
 }
---
+++ src/traits.rs
@@ -0,0 +1,11 @@
+/// Used to get the number of seconds which represent a specific object which implements this trait.
+pub trait AsSeconds {
+    /// Get the duration time in seconds
+    fn as_seconds(&self) -> u64;
+}
+
+/// Used to get the number of full minutes which represent a specific object which implements this trait.
+pub trait AsMinutes {
+    /// Get the duration time in full minutes
+    fn as_minutes(&self) -> u64;
+}
